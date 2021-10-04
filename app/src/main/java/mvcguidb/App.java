ad7a530bd7ec66e23e0d1cab649d072f5dc08e7f
Okamii <fahadhafiz1998@gmail.com>
2021-10-04 23:54:49 +0100

    Added template for MVC

--- app/src/main/java/mvcguidb/App.java
+++ app/src/main/java/mvcguidb/App.java
@@ -27,5 +27,26 @@ public class App extends Application {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         launch();
-    }
+      //fetch student record based on his roll no from the database
+      Student model  = retriveStudentFromDatabase();
+
+      //Create a view : to write student details on console
+      StudentView view = new StudentView();
+
+      StudentController controller = new StudentController(model, view);
+
+      controller.updateView();
+
+      //update model data
+      controller.setStudentName("John");
+
+      controller.updateView();
+   }
+
+   private static Student retriveStudentFromDatabase(){
+      Student student = new Student();
+      student.setName("Robert");
+      student.setRollNo("10");
+      return student;
+   }
 }
